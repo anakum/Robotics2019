532dd9b03ce708e16b01e45b2a2e91e79deeada8
Christian Sparks <csparks1919@hotmail.com>
2019-01-14 17:43:09 -0600

    Changed solenoid control to Jaguar Motor

--- src/org/usfirst/frc/team4787/robot/commands/CannonFire.java
+++ src/org/usfirst/frc/team4787/robot/commands/CannonFire.java
@@ -26,7 +26,6 @@ public class CannonFire extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -34,15 +33,21 @@ public class CannonFire extends Command {
       long tEnd = System.currentTimeMillis();
       long tDelta = tEnd - tStart;
       double elapsedSeconds = tDelta / 1000.0;
-      if (elapsedSeconds > 0.3) {
+      if (elapsedSeconds > 0.2) {
     	  return true;
+      }
+      else if(elapsedSeconds > 0.15) {
+        Robot.m_Cannon.set(false);
+      }
+      else if(elapsedSeconds > 0.05) {
+        Robot.m_Cannon.stop()
       }
       return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-      Robot.m_Cannon.set(false);
+      Robot.m_Cannon.stop();
     }
 
     // Called when another command which requires one or more of the same
--- src/org/usfirst/frc/team4787/robot/subsystems/SolenoidSubsystem.java
+++ src/org/usfirst/frc/team4787/robot/subsystems/SolenoidSubsystem.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4787.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 import org.usfirst.frc.team4787.robot.RobotMap;
@@ -9,17 +10,27 @@ import org.usfirst.frc.team4787.robot.RobotMap;
  */
 public class SolenoidSubsystem extends Subsystem {
 
-	private Solenoid sol; 
-	
+	//private Solenoid sol; 
+	private Jaguar sol_controller
+
 	public SolenoidSubsystem(int solenoidPin) {
 		super();
 		
-		sol = new Solenoid(solenoidPin);
+		sol_controller = new Jaguar(solenoidPin);
 	}
 	
-	public void set(boolean value) {
-		sol.set(value);
+	public void set(boolean isOn) {
+		if(isOn){
+      sol_controller.set(1);
+    }
+    else {
+      sol_controller.set(-1)
+    }
 	}
+
+  public void stop() {
+    sol_controller.set(0)
+  }
 	
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
